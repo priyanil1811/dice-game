6eb8d5db1d553c7aaf6b9698e65bedb5d6b3b7f0
priyanka <priya.nil1811@gmail.com>
2020-12-03 21:07:41 -0500

    dice game exercise

--- js/dice.js
+++ js/dice.js
@@ -2,3 +2,15 @@
 function getRandomDiceRoll(sides=6) {
   return Math.floor( Math.random() * sides ) + 1
 }
+
+//Math.random() * sides ) + 1
+//after * whatever number you put that is maximumm number 
+//after + whatever number you put that is minimumm number
+//you will get number between of those number
+//*6 +1 you will get 1 to 6 number randomly
+
+
+
+
+//another practice exc
+// URL:  github.com/juneate/background-generator
--- js/script.js
+++ js/script.js
@@ -1,5 +1,94 @@
+//search for the msg and replace it 
+/*let diceRoll = getRandomDiceRoll()
+
+let theMsg = document.querySelector(`.msg`)
+theMsg.textContent = `You rolled: ${diceRoll}`
+
+// replace src and alt attribute
+//Option: 1     (Better way)
+let theFace = document.querySelector(`.face`)
+theFace.setAttribute(`src` , `img/dice${diceRoll}.svg`)
+theFace.setAttribute(`alt` , `Rolled ${diceRoll}`)
+
+//or
+//OPTION 2:
+// replace "dice" div (innerHTML)
+
+let theDice = document.querySelector(`.dice`)
+theDice.innerHTML = `<img src="img/dice${diceRoll}.svg" , alt="Rolled ${diceRoll}" class="face">`*/
+
+
+let theMsg = document.querySelector(`.msg`)
+let theFace = document.querySelector(`.face`)
+let theFaceOne = document.querySelector(`.face-1`)
+ 
+let theRollDice = function() {
+    let diceRoll = getRandomDiceRoll()
+    let diceRoll2 = getRandomDiceRoll()
+    let totalRoll = diceRoll + diceRoll2
+    let textMsg = ``
+    if (totalRoll === 2) {
+         textMsg = `Snake Eyes`
+    }
+    else if (totalRoll === 2) {
+        textMsg = `Snake Eyes`
+   }
+    else if (totalRoll === 3) {
+    textMsg = `Ace Deuce`
+    }
+    else if (diceRoll === 2 && diceRoll2 === 2) {
+        textMsg = `Hard Four`
+    }
+    else if (totalRoll === 4) {
+        textMsg = `Easy Four`
+    }
+    else if (totalRoll === 5) {
+        textMsg = `Five (Fever Five)`
+    }
+    else if (diceRoll === 3 && diceRoll2 === 3) {
+        textMsg = `Hard Six`
+    }
+    else if (totalRoll === 6) {
+        textMsg = `Easy Six`
+    }
+    else if (totalRoll === 7) {
+        textMsg = `Natural or Seven Out`
+    }
+    else if (diceRoll === 4 && diceRoll2 === 4) {
+        textMsg = `Hard Eight`
+    }
+    else if (totalRoll === 8) {
+        textMsg = `Easy Eight`
+    }
+    else if (totalRoll === 9) {
+        textMsg = `Nine (Nina)`
+    }
+    else if (diceRoll === 5 && diceRoll2 === 5) {
+        textMsg = `Hard Ten`
+    }
+    else if (totalRoll === 10) {
+        textMsg = `Easy Ten`
+    }
+    else if (totalRoll === 11) {
+        textMsg = `Yo (Yo-leven)`
+    }
+    else if (totalRoll === 12) {
+        textMsg = `Boxcars or Midnight` 
+    }
+    else {
+         textMsg = totalRoll
+    }
+
+    theMsg.textContent = `You rolled: ${textMsg}` 
+    theFace.setAttribute(`src` , `img/dice${diceRoll}.svg`)
+    theFace.setAttribute(`alt` , `Rolled ${diceRoll}`) 
+    theFaceOne.setAttribute(`src` , `img/dice${diceRoll2}.svg`)
+    theFaceOne.setAttribute(`alt` , `Rolled ${diceRoll2}`) 
+}
+
+
 // Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
+//console.log(getRandomDiceRoll())
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
 
@@ -8,3 +97,5 @@ console.log(getRandomDiceRoll())
 // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
 
 // 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
+
+
